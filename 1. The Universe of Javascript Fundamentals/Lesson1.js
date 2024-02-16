61f97851ca389cf2e13b3404deb225b47db98540
Tshupane Morake <40323964+Papase14@users.noreply.github.com>
2024-02-16 22:08:45 +0200

    Lesson 1

---
+++ 1. The Universe of Javascript Fundamentals/Lesson1.js
@@ -0,0 +1,28 @@
+// Outputting text to the console
+console.log("Hello World");
+
+// Comenting in Javascript
+// Single line comment
+
+/** 
+ * Multiple line comment
+*/
+
+// Understanding Data Types in Javascript
+console.log(42); // prints  out number 42
+console.log("Learning Javascript"); //  prints "Learning Javascript" as a string
+console.log(true); //  prints true
+
+// Making Javascript Expressive
+console.log(2 + 2); // Outputs: 4
+console.log("2 + 2"); // Outputs: 2 + 2
+console.log("Hello, " + "World!"); // Outputs: Hello, World!
+console.log("2" + "2"); // Outputs: 22, because + operation applied on strings concatenates them
+console.log(2 + 2 > 5); // Outputs: false, because 2 + 2 is not greater than 5.
+
+
+// Comparison operators
+console.log(5 < 6);   // true
+console.log(5 > 6);   // false
+console.log(5 <= 5);  // true
+console.log(5 >= 10); // false
